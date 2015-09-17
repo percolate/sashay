fa4baa6d28624e195bbc38fc26a20074d66c2563
Christopher Cliff <chris@percolate.com>
2015-09-17 16:43:06 -0500

    Refactor API.

--- lib/components/main.jsx
+++ lib/components/main.jsx
@@ -12,19 +12,13 @@ module.exports = React.createClass({
         PureRenderMixin,
     ],
     propTypes: {
+        basePath: React.PropTypes.string,
         host: React.PropTypes.string,
         info: React.PropTypes.shape({
             title: React.PropTypes.string.isRequired,
         }).isRequired,
-        methodGroups: React.PropTypes.array.isRequired,
+        groups: React.PropTypes.array.isRequired,
         schemes: React.PropTypes.array,
-    },
-
-    getDefaultProps: function () {
-        return {
-            host: 'percolate.com',
-            schemes: ['https'],
-        }
     },
 
     render: function () {
@@ -33,55 +27,71 @@ module.exports = React.createClass({
                 <section>
                     <article>
                         <h1>{this.props.info.title}</h1>
-                        <h2>Methods</h2>
                     </article>
                     <aside />
                 </section>
-                {_.map(this.props.methodGroups, function (methodGroup) {
+                {_.map(this.props.groups, function (group) {
                     return (
-                        <div key={methodGroup.slug}>
+                        <div key={group.name}>
                             <section
-                                id={methodGroup.slug}
-                                ref={methodGroup.slug}
+                                id={group.name}
+                                ref={group.name}
                             >
                                 <article>
-                                    <h3>{methodGroup.name}</h3>
+                                    <h3>{group.description}</h3>
                                 </article>
                                 <aside />
                             </section>
-                            {_.map(methodGroup.methods, function (method) {
+                            {_.map(group.operations, function (operation) {
                                 return (
-                                    <section
-                                        id={method.slug}
-                                        key={method.slug}
-                                        ref={method.slug}
+                                    <div
+                                        id={operation.slug}
+                                        key={operation.slug}
+                                        ref={operation.slug}
                                     >
-                                        <article>
-                                            <h4>{method.name}</h4>
-                                            <div dangerouslySetInnerHTML={{
-                                                __html: marked(method.description),
-                                            }} />
-                                            {(!_.isEmpty(method.parameters))
-                                                ?
-                                                    <Parameters parameters={method.parameters} />
-                                                :
-                                                    undefined
-                                            }
-                                        </article>
-                                        <aside>
-                                            <h5>Definition</h5>
-                                            <pre>
-                                                <code>{[
-                                                    method.verb.toUpperCase(),
-                                                    url.format({
-                                                        host: this.props.host,
-                                                        pathname: method.pathname,
-                                                        protocol: _.first(this.props.schemes),
-                                                    }),
-                                                ].join(' ')}</code>
-                                            </pre>
-                                        </aside>
-                                    </section>
+                                        <section>
+                                            <article>
+                                                <h4>{operation.summary}</h4>
+                                                {!_.isEmpty(operation.description) && (
+                                                    <div dangerouslySetInnerHTML={{
+                                                        __html: marked(operation.description),
+                                                    }} />
+                                                )}
+                                                {(!_.isEmpty(operation.parameters)) && (
+                                                    <Parameters parameters={operation.parameters} />
+                                                )}
+                                                {!_.isEmpty(operation.response) && (
+                                                    <div>
+                                                        <h4>Returns</h4>
+                                                        <div dangerouslySetInnerHTML={{
+                                                            __html: marked(operation.response.description),
+                                                        }} />
+                                                    </div>
+                                                )}
+                                            </article>
+                                            <aside>
+                                                <h5>Definition</h5>
+                                                <pre>
+                                                    <code>{[
+                                                        operation.verb.toUpperCase(),
+                                                        url.format({
+                                                            host: this.props.host,
+                                                            pathname: [this.props.basePath, operation.path].join(''),
+                                                            protocol: _.first(this.props.schemes),
+                                                        }),
+                                                    ].join(' ')}</code>
+                                                </pre>
+                                                {!_.isEmpty(operation.response) && (
+                                                    <div>
+                                                        <h5>Example response</h5>
+                                                        <pre>
+                                                            <code>{JSON.stringify(operation.response.example, undefined, 2)}</code>
+                                                        </pre>
+                                                    </div>
+                                                )}
+                                            </aside>
+                                        </section>
+                                    </div>
                                 )
                             }, this)}
                         </div>
--- lib/components/parameters.jsx
+++ lib/components/parameters.jsx
@@ -10,15 +10,33 @@ module.exports = React.createClass({
     },
 
     render: function () {
+        var parameters = _.chain(this.props.parameters)
+            .map(function (parameter) {
+                if (_.has(parameter, 'schema')) return getParametersFromSchema(parameter.schema)
+                return _.extend(parameter, {
+                    type: getType(parameter),
+                })
+            })
+            .flatten()
+            .value()
         return (
             <div>
                 <h6>Parameters</h6>
                 <ul className="parameters">
-                    {_.map(this.props.parameters, function (parameter, i) {
+                    {_.map(parameters, function (parameter, i) {
                         return (
-                            <li key={i}>
+                            <li
+                                className="parameter"
+                                key={i}
+                            >
                                 <div className="parameter-spec">
                                     <div>{parameter.name}</div>
+                                    <div className="parameter-info">
+                                        <span>{parameter.type}</span>
+                                        {(!_.isEmpty(parameter.default)) && (
+                                            <span>, default is <strong><code>{JSON.stringify(parameter.default)}</code></strong></span>
+                                        )}
+                                    </div>
                                     {(parameter.required)
                                         ?
                                             <div className="parameter-required">Required</div>
@@ -47,3 +65,19 @@ module.exports = React.createClass({
     },
 
 })
+
+function getParametersFromSchema (schema) {
+    return _.chain(schema.properties)
+        .map(function (property, name) {
+            return _.extend(property, {
+                name: name,
+                required: _.contains(schema.required, name),
+                type: getType(property),
+            })
+        })
+        .value()
+}
+
+function getType (property) {
+    return (property.type === 'array') ? ['[', ']'].join(property.items.type) : property.type
+}
