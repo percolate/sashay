684ff2e492ddcc2b925f7d9883ca2d550eda4d3f
Miroslav Rusin <miroslav.rusin@hotovo.org>
2016-04-18 10:59:45 +0200

    API-19 fixes after CI

--- lib/ui/components/controller.jsx
+++ lib/ui/components/controller.jsx
@@ -7,7 +7,7 @@ var ReactDom = require('react-dom')
 
 var IS_BROWSER = process.env.PLATFORM === 'browser'
 
-if (IS_BROWSER) require('../less/index.less')    
+if (IS_BROWSER) require('../less/index.less')
 
 module.exports = React.createClass({
 
