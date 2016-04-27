bc49d1c3050284224071d21fb1b5e131e4a8a7b7
Miroslav Rusin <miroslav.rusin@hotovo.org>
2016-04-27 10:50:31 +0200

    API-60 refactoring, adding content-type header

--- lib/ui/components/main.jsx
+++ lib/ui/components/main.jsx
@@ -158,8 +158,7 @@ module.exports = React.createClass({
                     {_.has(method, 'method') && (
                         <section>
                             <h1>Example curl request</h1>
-                                <Code lang="sh" code={helper.getCurl(exampleAbsoluteUri, method.method.toUpperCase(), 'YOUR_API_KEY',
-                                  _.has(body, 'example') ? JSON.parse(_.get(body, 'example')) : null)}
+                                <Code lang="sh" code={helper.getCurl(exampleAbsoluteUri, method.method.toUpperCase(), 'YOUR_API_KEY')}
                                 />
                         </section>
                     )}
