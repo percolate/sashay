79cd6ba9f43490d9d91e2c8a43f7095094f8f60b
Miroslav Rusin <miroslav.rusin@hotovo.org>
2016-05-05 14:09:27 +0200

    API-69 navigation works after resizing

--- lib/ui/components/controller.jsx
+++ lib/ui/components/controller.jsx
@@ -43,6 +43,7 @@ module.exports = React.createClass({
         this._updateHash(true)
         window.addEventListener('scroll', _.debounce(this._updateHash, 20))
         window.addEventListener('resize', _.debounce(this._updateHash, 20))
+        window.addEventListener('resize', _.debounce(this._updateOffsets, 20))
     },
 
     componentWillUnmount: function () {
