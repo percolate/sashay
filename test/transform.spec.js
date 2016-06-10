2a78147b13d86968339f1f904d6241a968adfe9a
Miroslav Rusin <miroslav.rusin@hotovo.org>
2016-06-10 06:27:41 +0200

    API-123 fixes after review

--- test/transform.spec.js
+++ test/transform.spec.js
@@ -196,12 +196,12 @@ describe('transform()', function () {
                         {
                             contents: [
                                 {
-                                    text: 'Percolate API is generated from RAML (Restful APIs Markup Language), a human and machine readable API definition enabling the creation of automated and reusable ecosystems of tools.\n\nThe Percolate API RAML definition will help you to automate your work interacting with Percolate API, importing it into testing tools (like Postman or Paw) or monitoring tools (like SoapUI, Runscope or APIscience) and work efficiently with the large ecosystem or RAML plugins.\n\nDownload the Percolate RAML file [here](index.raml).',
+                                    text: 'Percolate API is generated from RAML (Restful APIs Markup Language), a human and machine readable API definition enabling the creation of automated and reusable ecosystems of tools.\n        The Percolate API RAML definition will help you automate your work interacting with Percolate API, importing it into testing tools (like Postman or Paw) or monitoring tools (like SoapUI, Runscope or APIscience) and work efficiently with the large ecosystem or RAML plugins.\n        Download the Percolate RAML file [here](index.raml).',
                                     type: 'text',
                                 }
                             ],
-                            displayName: 'Download RAML',
-                            slug: 'topic.download.raml',
+                            displayName: 'Download',
+                            slug: 'topic.download',
                         },
                     ],
                     version: 'foo',
