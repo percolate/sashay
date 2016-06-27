80a831a20602a465861171e25abd9c9ea233a443
Guillaume Mathieu <contact@gmathieu.com>
2016-06-26 18:46:50 -0700

    restored parameters

--- lib/ui/components/parameters.jsx
+++ lib/ui/components/parameters.jsx
@@ -1,58 +1,71 @@
 var _ = require('lodash')
 var Markdown = require('./markdown.jsx')
-var Parameter = require('./parameter.jsx')
 var PureRenderMixin = require('react-addons-pure-render-mixin')
 var React = require('react')
-var ReactDOM = require('react-dom')
-
-var ROOT = 'root'
 
 module.exports = React.createClass({
-    expanded: false,
+
     displayName: 'Parameters',
     mixins: [
         PureRenderMixin,
     ],
     propTypes: {
-        parameters: React.PropTypes.shape({
-            description: React.PropTypes.string,
-            default: React.PropTypes.string,
-            displayName: React.PropTypes.string,
-            enum: React.PropTypes.array,
-            pattern: React.PropTypes.string,
-            properties: React.PropTypes.object,
-            required: React.PropTypes.bool,
-            type: React.PropTypes.any,
-        }),
-        onChange: React.PropTypes.func.isRequired,
-        onClick: React.PropTypes.func,
+        parameters: React.PropTypes.object.isRequired,
     },
 
     render: function () {
-        var parameters = _.chain(this.props.parameters)
-            .map(function (parameter, key) {
-                parameter.displayName = key
-                return parameter
-            })
-            .sortBy(function (parameter) {
-                return parameter.displayName
-            })
-            .value()
+        var parameters = this.props.parameters
+
         return (
             <div>
                 <ul className="parameters">
-                    {_.map(parameters, function (parameter, key) {
+                    {_.map(parameters, function (parameter, i) {
                         return (
                             <li
                                 className="parameter"
-                                key={key}
+                                key={i}
                             >
-                                <Parameter parameter={parameter} onChange={this.props.onChange} onClick={this.props.onClick}/>
+                                <div className="parameter-spec">
+                                    <div>{parameter.displayName}</div>
+                                    <div className="parameter-info">
+                                        <div>{parameter.type}</div>
+                                        {(_.toString(parameter.default) !== '') && (
+                                            <div className="parameter-info parameter-default">
+                                                default is <strong>{JSON.stringify(parameter.default)}</strong>
+                                            </div>
+                                        )}
+                                    </div>
+                                    {(parameter.required)
+                                        ?
+                                            <div className="parameter-required">Required</div>
+                                        :
+                                            undefined
+                                    }
+                                </div>
+                                <div className="parameter-desc">
+                                    {(!_.isEmpty(parameter.description))
+                                        ?
+                                            <Markdown content={parameter.description} />
+                                        :
+                                            undefined
+                                    }
+                                    {!_.isNil(parameter.enum) && (
+                                        <span>
+                                            Allowed values: [{parameter.enum.join(', ')}]
+                                        </span>
+                                    )}
+                                    {!_.isNil(parameter.pattern) && (
+                                        <span>
+                                            Pattern: {parameter.pattern}
+                                        </span>
+                                    )}
+                                </div>
                             </li>
                         )
-                    }.bind(this))}
+                    }, this)}
                 </ul>
             </div>
         )
     },
+
 })
